efed2037995341bdf5bcf297a15a528d7a58b816
josecham <gabriel_influencia74@hotmail.com>
2023-10-01 10:17:27 -0300

    la etiqueta de lo que guardo

--- js/index.js
+++ js/index.js
@@ -8,3 +8,29 @@ function datos(event){
     localStorage.setItem("su correo es: ", correo);
 }
 
+//
+$('#dataStringExample button').on('click', function() {
+    var email = $('#dataStringExample input[name="email"]').val();
+    var password = $('#dataStringExample input[name="password"]').val();
+    var dataString = 'email='+email+'&password='+password;
+
+    alert('Enviando datos con dataString!\r\rInformación antes de enviar a Ajax:\r\r' + dataString);
+
+    $.ajax({
+        type: 'POST',
+        url: 'process.php',
+        data: dataString,
+        dataType: 'json',
+        success: function(response) {
+            var message = `
+                La información ha sido recibida en el servidor!\r\r
+                Email: ${response.email}\r
+                Contrseña: ${response.password}\r\r
+                Token: ${response.password_md5}\r\r
+                Gracias!
+            `;
+            alert(message);
+        }
+    });
+});
+
